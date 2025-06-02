b1f4172e3b655fb9ff287bc13c1a0436f9db7523
Charles Stoeter <charlesstoeter@gmail.com>
2025-06-02 16:22:20 -0400

    initializations and framework

--- CharlesStoeterLab8/CharlesStoeterLab8.cpp
+++ CharlesStoeterLab8/CharlesStoeterLab8.cpp
@@ -1,8 +1,40 @@
-
-
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+
+
+const int SCREEN_W = 900;
+const int SCREEN_H = 800;
+
+
+
+int main() {
+
+
+
+    // Initialize Allegro
+    if (!al_init()) {
+        std::cerr << "Failed to initialize Allegro!\n";
+        return -1;
+    }
+
+    // Create the display
+    ALLEGRO_DISPLAY* display = al_create_display(SCREEN_W, SCREEN_H);
+    if (!display) {
+        std::cerr << "Failed to create display!\n";
+        return -1;
+    }
+
+
+
+
+    // Keep window open for now
+    al_rest(3.0); // Show display for 3 seconds (for test)
+
+    // Clean up
+    al_destroy_display(display);
+
+    return 0;
 }
